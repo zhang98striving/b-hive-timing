5ac4144af3d5e837212f1aac4a69035eb1eaa651
P. A. Jung <alexander.jung@rwth-aachen.de>
2023-08-10 19:23:05 +0200

    bug fixes, better legend ROC curves

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -8,7 +8,7 @@ import mplhep as hep
 import numpy as np
 import os
 
-plt.style.use(hep.cms.style.ROOT)
+plt.style.use(hep.cms.style.CMS)
 
 
 class PlottingTask(MainBaseTask):
@@ -19,7 +19,6 @@ class PlottingTask(MainBaseTask):
         return self.local_target("loss.pdf")
 
     def run(self):
-        # input = np.load(self.output_directory + "/input.npy", allow_pickle=True)
         files = np.array(
             open(f"{self.output_directory}/processed_files.txt", "r").read().split("\n")[:-1]
         )
@@ -82,9 +81,9 @@ def prepare_roc(input_directory, output_directory, dataset_keys, truth, output_d
         l_pred = output_data_[:, -2:].sum(axis=1)
 
         
-        bvsl = np.where((b_pred + l_pred)>=0, (b_pred)/(b_pred + l_pred), -1)
-        cvsb = np.where((b_pred + c_pred)>=0, (c_pred)/(b_pred + c_pred), -1)
-        cvsl = np.where((l_pred + c_pred)>=0, (c_pred)/(l_pred + c_pred), -1)
+        bvsl = np.where((b_pred + l_pred)>0, (b_pred)/(b_pred + l_pred), -1)
+        cvsb = np.where((b_pred + c_pred)>0, (c_pred)/(b_pred + c_pred), -1)
+        cvsl = np.where((l_pred + c_pred)>0, (c_pred)/(l_pred + c_pred), -1)
         
         b_veto = ((truth_ != 0) | (truth_ != 1) | (truth_ != 2) | (summed_jets != 0))[(jet_pt_ > pt_min) | (jet_pt_ < pt_max)]
         c_veto = ((truth_ != 3) | (summed_jets != 0))[(jet_pt_ > pt_min) | (jet_pt_ < pt_max)]
@@ -92,30 +91,11 @@ def prepare_roc(input_directory, output_directory, dataset_keys, truth, output_d
 
         roc_list   = []
         label_list = ["BvsL", "CvsB", "CvsL"]
-        roc_list.append(calculate_roc(b_jets, bvsl, c_veto, output_directory, key, label_list[0].lower()))
-        roc_list.append(calculate_roc(c_jets, cvsb, l_veto, output_directory, key, label_list[1].lower()))
-        roc_list.append(calculate_roc(c_jets, cvsl, b_veto, output_directory, key, label_list[2].lower()))
+        roc_list.append(calculate_roc(b_jets, bvsl, c_veto, output_directory, key, label_list[0]))
+        roc_list.append(calculate_roc(c_jets, cvsb, l_veto, output_directory, key, label_list[1]))
+        roc_list.append(calculate_roc(c_jets, cvsl, b_veto, output_directory, key, label_list[2]))
 
         plot_roc(roc_list, label_list, key, pt_min, pt_max, output_directory)
-
-# adapted from https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.py and https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.ipynb
-def plot_roc(roc_list, label_list, dataset_key, pt_min, pt_max, output_directoy):
-    for i, l in enumerate(label_list):
-        fpr, tpr, auc = roc_list[i]
-        
-        plt.figure()
-        plt.plot(tpr, fpr, label=f"AUC = {np.round(auc, 3)}", color="blue")
-        plt.title(f"{l}, ${pt_min}GeV < p_T < {pt_max}GeV$, {dataset_key} events")
-        plt.xlabel("Tagging efficiency")
-        plt.ylabel("Mistagging rate")
-        plt.yscale("log")
-        plt.xlim(0, 1)
-        plt.ylim(1e-3, 1)
-        plt.grid(which="minor", alpha=0.85)
-        plt.grid(which="major", alpha=0.95, color="black")
-        plt.legend(loc="best")
-        plt.savefig(f"{output_directoy}roc_{dataset_key}_{l.lower()}.pdf")
-        plt.close()
 
 # adapted from https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.py and https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.ipynb    
 def calculate_roc(truth, dicriminator, veto, output_directory, dataset_key, name):
@@ -125,8 +105,35 @@ def calculate_roc(truth, dicriminator, veto, output_directory, dataset_key, name
     fpr   = np.asarray([fpr[i] for i in sorted(index)])
     tpr   = np.asarray([tpr[i] for i in sorted(index)])
     area  = auc(fpr, tpr)
-    np.save(f"{output_directory}roc_{dataset_key}_{name}.npy", np.array([fpr, tpr, area], dtype=object))
+    np.save(f"{output_directory}roc_{dataset_key.lower()}_{name.lower()}.npy", np.array([fpr, tpr, area], dtype=object))
     return fpr, tpr, area
+
+# adapted from https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.py and https://github.com/AlexDeMoor/DeepJet/blob/ParticleTransformer/scripts/plot_roc.ipynb
+def plot_roc(roc_list, label_list, dataset_key, pt_min, pt_max, output_directoy):
+    if dataset_key=="TT":
+        events_text = rf"$t\bar{{t}}$"
+    else: 
+        events_text = "QCD"
+    equation_text = [rf"$\frac{{P(b) + P(bb) + P(leptb)}}{{P(b) + P(bb) + P(leptb) + P(uds) + P(g)}}$", rf"$\frac{{P(c)}}{{P(c) + P(b) + P(bb) + P(leptb)}}$", rf"$\frac{{P(c)}}{{P(c) + P(uds) + P(g)}}$"]
+    pt_text = rf"${pt_min} \leq p_T \leq {pt_max}\,GeV$"
+    eta_text = rf"$|\eta| \leq 2.5$"
+
+    for i, l in enumerate(label_list):
+        fpr, tpr, auc = roc_list[i]
+        
+        plt.figure()
+        plt.plot(tpr, fpr, label=f"{l} DeepJet \n" + rf"AUC ${{\approx}}$ {np.round(auc, 3)}", color="blue")
+        plt.xlabel("Tagging efficiency")
+        plt.ylabel("Mistagging rate")
+        plt.yscale("log")
+        plt.xlim(0, 1)
+        plt.ylim(1e-3, 1)
+        plt.grid(which="minor", alpha=0.85)
+        plt.grid(which="major", alpha=0.95, color="black")
+        plt.legend(title=f" {events_text} jets \n {pt_text} \n {eta_text}", loc="best", alignment="left", title_fontsize=15, fontsize=15)
+        hep.cms.label("Preliminary", com=13)
+        plt.savefig(f"{output_directoy}roc_{dataset_key}_{l.lower()}.pdf")
+        plt.close()
 
 
 def plot_losses(train_loss, test_loss, output_dir):
