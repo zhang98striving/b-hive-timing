57c47d6d5f761be0f44d49991f8680c23ad39fc6
Niclas Eich <niclas.eich@rwth-aachen.de>
2023-08-21 15:13:51 +0200

    got rid of obsolete try-except

--- tasks/dataset.py
+++ tasks/dataset.py
@@ -34,141 +34,135 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
         os.makedirs(self.local_path(), exist_ok=True)
         output_string = ""
         np.random.seed(1)
-        try:
-            for sample_prefix in ["training", "test"]:
-                path = self.training_dataset_path if "training" else self.test_dataset_path
-                samples = open(path, "r").read().split("\n")[:-1]
+        for sample_prefix in ["training", "test"]:
+            path = self.training_dataset_path if "training" else self.test_dataset_path
+            samples = open(path, "r").read().split("\n")[:-1]
 
-                # Get all dataset name prefixes:
-                l = []
-                for ti in samples:
-                    dataset_name = ti.split("_TuneCP5")[0].split("/")[-1]
-                    if dataset_name not in l:
-                        l.append(dataset_name)
+            # Get all dataset name prefixes:
+            l = []
+            for ti in samples:
+                dataset_name = ti.split("_TuneCP5")[0].split("/")[-1]
+                if dataset_name not in l:
+                    l.append(dataset_name)
 
-                # Make a dictionary entry for all of them:
-                sample_dict = {}
-                for li in l:
-                    mask = np.core.defchararray.find(samples, li) != -1
-                    sample_dict[sample_prefix + "_" + li] = np.array(samples)[mask].tolist()
+            # Make a dictionary entry for all of them:
+            sample_dict = {}
+            for li in l:
+                mask = np.core.defchararray.find(samples, li) != -1
+                sample_dict[sample_prefix + "_" + li] = np.array(samples)[mask].tolist()
 
-                futures_run = processor.Runner(
-                    executor=processor.FuturesExecutor(
-                        compression=None, workers=self.coffea_worker
-                    ),
-                    schema=BaseSchema,
-                    chunksize=10000,
-                )
-                output = futures_run(
-                    sample_dict,
-                    "DeepJetNTupler/DeepJetvars",
-                    processor_instance=DeepJet_NTupleDataPreprocessing(
-                        self.local_path(), config_dict, ""
-                    ),
-                )
+            futures_run = processor.Runner(
+                executor=processor.FuturesExecutor(compression=None, workers=self.coffea_worker),
+                schema=BaseSchema,
+                chunksize=10000,
+            )
+            output = futures_run(
+                sample_dict,
+                "DeepJetNTupler/DeepJetvars",
+                processor_instance=DeepJet_NTupleDataPreprocessing(
+                    self.local_path(), config_dict, ""
+                ),
+            )
 
-                # saving histograms from coffea
-                histograms = []
-                file_list = []
-                for key in output.keys():
-                    if key == "output_location":
-                        for line in output["output_location"]:
-                            file_list.append(f"{line}")
-                    else:
-                        output_location = f"{self.local_path()}/{key}.npy"
-                        np.save(output_location, output[key])
-                        histograms.append(output[key])
-                np.save(
-                    self.output()[f"histogram_{sample_prefix}"].path,
-                    np.array(histograms),
-                )
-
-                print(f"number of output {sample_prefix} files:", len(file_list))
-
-                np.save(self.output()["config_dict"].path, config_dict)
-                files = np.array(file_list)
-                np.random.shuffle(files)
-                chunk_size = 100000
-                dim = np.load(files[0], allow_pickle=True).shape[-1]
-                chunk = np.empty((chunk_size, dim))
-                N_tot = np.load(
-                    self.output()[f"histogram_{sample_prefix}"].path,
-                ).sum()
-                i = 0
-                j = 0
-                Ns = 0
-                n_chunk = 0
-                # Merge datasets for the training set:
-                if sample_prefix == "training":
-                    N_s = [N_tot]
-                    labels = ["training"]
-                    for file in track(files, "Merging..."):
-                        data = np.load(file, allow_pickle=True)
-                        n_samples = data.shape[0]
-                        # chunk overflow:
-                        if n_chunk + n_samples > chunk_size:
-                            index_range = chunk_size - n_chunk
-                        else:
-                            index_range = n_samples
-                        # Category overflow:
-                        if n_samples + Ns > N_s[i]:
-                            index_range = N_s[i] - Ns
-                        Ns += index_range
-                        chunk[n_chunk : n_chunk + index_range] = data[:index_range]
-                        n_chunk += index_range
-                        if n_chunk == chunk_size or Ns == N_s[i]:
-                            filename = f"{self.local_path()}/{labels[i]}_{j}.npy"
-                            np.save(filename, chunk[:n_chunk])
-                            output_string += f"{filename}\n"
-                            j += 1
-                            chunk = np.zeros((chunk_size, dim))
-                            chunk[: n_samples - index_range] = data[index_range:]
-                            n_chunk = n_samples - index_range
-                            if Ns == N_s[i]:
-                                i += 1
-                                Ns = 0
-                                j = 0
-                            Ns += n_samples - index_range
+            # saving histograms from coffea
+            histograms = []
+            file_list = []
+            for key in output.keys():
+                if key == "output_location":
+                    for line in output["output_location"]:
+                        file_list.append(f"{line}")
                 else:
-                    N_s = [int(0.5 * N_tot), N_tot - int(0.5 * N_tot)]
-                    labels = ["test", "validation"]
-                    data_origin = ""
-                    for file in track(files, "Merging..."):
-                        data = np.load(file, allow_pickle=True)
-                        n_samples = data.shape[0]
-                        # chunk overflow:
-                        if n_chunk + n_samples > chunk_size:
-                            index_range = chunk_size - n_chunk
-                        else:
-                            index_range = n_samples
-                        # Category overflow:
-                        if n_samples + Ns > N_s[i]:
-                            index_range = N_s[i] - Ns
-                        Ns += index_range
-                        chunk[n_chunk : n_chunk + index_range] = data[:index_range]
-                        data_origin += f"{file}\n" * index_range
-                        n_chunk += index_range
-                        if n_chunk == chunk_size or Ns == N_s[i]:
-                            filename = f"{self.local_path()}/{labels[i]}_{j}.npy"
-                            print(
-                                data_origin,
-                                file=open(".".join(filename.split(".")[:-1]) + ".txt", "w"),
-                            )
-                            data_origin = f"{file}\n" * (n_samples - index_range)
-                            np.save(filename, chunk[:n_chunk])
-                            output_string += f"{filename}\n"
-                            j += 1
-                            chunk = np.empty((chunk_size, dim))
-                            chunk[: n_samples - index_range] = data[index_range:]
-                            n_chunk = n_samples - index_range
-                            if Ns == N_s[i]:
-                                i += 1
-                                Ns = 0
-                                j = 0
-                            Ns += n_samples - index_range
-        except Exception as error:
-            print(error)
-            exit()
+                    output_location = f"{self.local_path()}/{key}.npy"
+                    np.save(output_location, output[key])
+                    histograms.append(output[key])
+            np.save(
+                self.output()[f"histogram_{sample_prefix}"].path,
+                np.array(histograms),
+            )
+
+            print(f"number of output {sample_prefix} files:", len(file_list))
+
+            np.save(self.output()["config_dict"].path, config_dict)
+            files = np.array(file_list)
+            np.random.shuffle(files)
+            chunk_size = 100000
+            dim = np.load(files[0], allow_pickle=True).shape[-1]
+            chunk = np.empty((chunk_size, dim))
+            N_tot = np.load(
+                self.output()[f"histogram_{sample_prefix}"].path,
+            ).sum()
+            i = 0
+            j = 0
+            Ns = 0
+            n_chunk = 0
+            # Merge datasets for the training set:
+            if sample_prefix == "training":
+                N_s = [N_tot]
+                labels = ["training"]
+                for file in track(files, "Merging..."):
+                    data = np.load(file, allow_pickle=True)
+                    n_samples = data.shape[0]
+                    # chunk overflow:
+                    if n_chunk + n_samples > chunk_size:
+                        index_range = chunk_size - n_chunk
+                    else:
+                        index_range = n_samples
+                    # Category overflow:
+                    if n_samples + Ns > N_s[i]:
+                        index_range = N_s[i] - Ns
+                    Ns += index_range
+                    chunk[n_chunk : n_chunk + index_range] = data[:index_range]
+                    n_chunk += index_range
+                    if n_chunk == chunk_size or Ns == N_s[i]:
+                        filename = f"{self.local_path()}/{labels[i]}_{j}.npy"
+                        np.save(filename, chunk[:n_chunk])
+                        output_string += f"{filename}\n"
+                        j += 1
+                        chunk = np.zeros((chunk_size, dim))
+                        chunk[: n_samples - index_range] = data[index_range:]
+                        n_chunk = n_samples - index_range
+                        if Ns == N_s[i]:
+                            i += 1
+                            Ns = 0
+                            j = 0
+                        Ns += n_samples - index_range
+            else:
+                N_s = [int(0.5 * N_tot), N_tot - int(0.5 * N_tot)]
+                labels = ["test", "validation"]
+                data_origin = ""
+                for file in track(files, "Merging..."):
+                    data = np.load(file, allow_pickle=True)
+                    n_samples = data.shape[0]
+                    # chunk overflow:
+                    if n_chunk + n_samples > chunk_size:
+                        index_range = chunk_size - n_chunk
+                    else:
+                        index_range = n_samples
+                    # Category overflow:
+                    if n_samples + Ns > N_s[i]:
+                        index_range = N_s[i] - Ns
+                    Ns += index_range
+                    chunk[n_chunk : n_chunk + index_range] = data[:index_range]
+                    data_origin += f"{file}\n" * index_range
+                    n_chunk += index_range
+                    if n_chunk == chunk_size or Ns == N_s[i]:
+                        filename = f"{self.local_path()}/{labels[i]}_{j}.npy"
+                        print(
+                            data_origin,
+                            file=open(".".join(filename.split(".")[:-1]) + ".txt", "w"),
+                        )
+                        data_origin = f"{file}\n" * (n_samples - index_range)
+                        np.save(filename, chunk[:n_chunk])
+                        output_string += f"{filename}\n"
+                        j += 1
+                        chunk = np.empty((chunk_size, dim))
+                        chunk[: n_samples - index_range] = data[index_range:]
+                        n_chunk = n_samples - index_range
+                        if Ns == N_s[i]:
+                            i += 1
+                            Ns = 0
+                            j = 0
+                        Ns += n_samples - index_range
 
         # Get the weights
         histograms = np.load(
