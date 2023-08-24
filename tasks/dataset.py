Precision down to float32; IterativeDataset overflow fixed; visual update

tasks/dataset.py
@@ -1,8 +1,15 @@
+import os
+import traceback
+
+import luigi
+import numpy as np
+from coffea import processor
 from coffea.nanoevents import BaseSchema, PFNanoAODSchema
-from tasks.base import BaseTask, config_dict
-from tasks.parameter_mixins import DatasetDependency
 from coffea.nanoevents.methods import base
 from rich.progress import track
+
+from tasks.base import BaseTask, config_dict
+from tasks.parameter_mixins import DatasetDependency
 from utils.processors import DeepJet_NTupleDataPreprocessing
 from coffea import processor
 import os
@@ -82,12 +89,10 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
                     for line in output["output_location"]:
                         file_list.append(f"{line}")
                 else:
-                    output_location = f"{self.local_path()}/{key}.npy"
-                    np.save(output_location, output[key])
                     histograms.append(output[key])
             np.save(
                 self.output()[f"histogram_{sample_prefix}"].path,
-                np.array(histograms),
+                np.array(histograms, dtype=np.float32),
             )
 
             print(f"number of output {sample_prefix} files:", len(file_list))
@@ -97,7 +102,7 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
             np.random.shuffle(files)
             chunk_size = 100000
             dim = np.load(files[0], allow_pickle=True).shape[-1]
-            chunk = np.empty((chunk_size, dim))
+            chunk = np.empty((chunk_size, dim), dtype=np.float32)
             N_tot = np.load(
                 self.output()[f"histogram_{sample_prefix}"].path,
             ).sum()
@@ -128,7 +133,7 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
                         np.save(filename, chunk[:n_chunk])
                         output_string += f"{filename}\n"
                         j += 1
-                        chunk = np.zeros((chunk_size, dim))
+                        chunk = np.zeros((chunk_size, dim), dtype=np.float32)
                         chunk[: n_samples - index_range] = data[index_range:]
                         n_chunk = n_samples - index_range
                         if Ns == N_s[i]:
@@ -165,7 +170,7 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
                         np.save(filename, chunk[:n_chunk])
                         output_string += f"{filename}\n"
                         j += 1
-                        chunk = np.empty((chunk_size, dim))
+                        chunk = np.empty((chunk_size, dim), dtype=np.float32)
                         chunk[: n_samples - index_range] = data[index_range:]
                         n_chunk = n_samples - index_range
                         if Ns == N_s[i]:
@@ -227,11 +232,3 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
             np.save(file, samples)
 
         self.output()["file_list"].dump(f"{output_string}", formatter="text")
-
-
-def empty_column_accumulator():
-    return processor.column_accumulator(np.array([], dtype=np.float64))
-
-
-def array_accumulator():
-    return processor.defaultdict_accumulator(empty_column_accumulator)

utils/torch/datasets.py
@@ -82,8 +82,9 @@ class DeepJetDataset(IterableDataset):
                 random_number = np.random.rand(s.shape[0])
                 goods = random_number > s[:, -2]
                 s = s[goods]
-            for i in range(s.shape[0]):
-                yield np.expand_dims(s[i, :-2], axis=-1), s[i, -2], s[i, -1]
+            for si in s:
+                yield np.expand_dims(si[:-2], axis=-1), si[-2], si[-1]
+        return None
 
     def get_all_weights(self):
         weights = np.empty((self.Nedges[-1]))

utils/torch/training.py
@@ -1,9 +1,16 @@
 import math
-import torch
-import numpy as np
-import torch.nn as nn
 
-from rich.progress import track
+import numpy as np
+import torch
+import torch.nn as nn
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 
 
 def perform_training(model, training_data, validation_data, directory, device, **kwargs):
@@ -14,7 +21,7 @@ def perform_training(model, training_data, validation_data, directory, device, *
     train_metrics = np.zeros((nepochs, 2))
     validation_metrics = np.zeros((nepochs, 2))
     for t in range(nepochs):
-        print(t, "of", nepochs)
+        print("Epoch", t + 1, "of", nepochs)
         loss_train, acc_train = train_model(
             training_data,
             model,
@@ -67,21 +74,41 @@ def train_model(
     losses = []
     accuracy = 0.0
     model.train()
-    it = 0
-    for x, w, y in track(dataloader, "Training..."):
-        pred = model(x.float().to(device))
-        loss = loss_fn(pred, y.type(torch.LongTensor).to(device)).mean()
 
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
+    with Progress(
+        TextColumn("{task.description}"),
+        TimeElapsedColumn(),
+        BarColumn(bar_width=None),
+        TaskProgressColumn(),
+        TimeRemainingColumn(),
+        TextColumn("0/? its"),
+        expand=True,
+    ) as progress:
+        N = 0
+        task = progress.add_task("Training...", total=dataloader.nits_expected)
+        for x, w, y in dataloader:
+            pred = model(x.float().to(device))
+            loss = loss_fn(pred, y.type(torch.LongTensor).to(device)).mean()
 
-        losses.append(loss.item())
-        accuracy += torch.sum(y.to(device) == pred.argmax(dim=1))
-        it += 1
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
 
+            losses.append(loss.item())
+            accuracy += torch.sum(y.to(device) == pred.argmax(dim=1))
+            N += x.shape[0]
+            progress.update(task, advance=1, description=f"Training...   | Loss: {loss:.2f}")
+            progress.columns[-1].text_format = "{}/{} its".format(
+                N // dataloader.batch_size,
+                "?"
+                if dataloader.nits_expected == len(dataloader)
+                else f"~{dataloader.nits_expected}",
+            )
+        progress.update(task, completed=dataloader.nits_expected)
+    dataloader.nits_expected = N // dataloader.batch_size
     accuracy /= len(dataloader.dataset)
-    print("  ", np.array(losses).mean(), float(accuracy))
+    print("  ", f"Average loss: {np.array(losses).mean():.2f}")
+    print("  ", f"Average accuracy: {float(accuracy):.2f}")
     return np.array(losses).mean(), float(accuracy)
 
 
@@ -89,13 +116,35 @@ def validate_model(dataloader, model, loss_fn, device="cpu"):
     losses = []
     accuracy = 0.0
     model.eval()
-    for x, w, y in track(dataloader, "Validating..."):
-        with torch.no_grad():
-            pred = model(x.float().to(device))
-            loss = loss_fn(pred, y.type(torch.LongTensor).to(device)).mean()
-            losses.append(loss.item())
+    with Progress(
+        TextColumn("{task.description}"),
+        TimeElapsedColumn(),
+        BarColumn(bar_width=None),
+        TaskProgressColumn(),
+        TimeRemainingColumn(),
+        TextColumn("0/? its"),
+        expand=True,
+    ) as progress:
+        N = 0
+        task = progress.add_task("Validation...", total=dataloader.nits_expected)
+        for x, w, y in dataloader:
+            with torch.no_grad():
+                pred = model(x.float().to(device))
+                loss = loss_fn(pred, y.type(torch.LongTensor).to(device)).mean()
+                losses.append(loss.item())
 
-            accuracy += torch.sum(y.to(device) == pred.argmax(dim=1))
+                accuracy += torch.sum(y.to(device) == pred.argmax(dim=1))
+            N += x.shape[0]
+            progress.update(task, advance=1, description=f"Validation... | Loss: {loss:.2f}")
+            progress.columns[-1].text_format = "{}/{} its".format(
+                N // dataloader.batch_size,
+                "?"
+                if dataloader.nits_expected == len(dataloader)
+                else f"~{dataloader.nits_expected}",
+            )
+        progress.update(task, completed=dataloader.nits_expected)
+    dataloader.nits_expected = N // dataloader.batch_size
     accuracy /= len(dataloader.dataset)
-    print("  ", np.array(losses).mean(), float(accuracy))
+    print("  ", f"Average loss: {np.array(losses).mean():.2f}")
+    print("  ", f"Average accuracy: {float(accuracy):.2f}")
     return np.array(losses).mean(), float(accuracy)