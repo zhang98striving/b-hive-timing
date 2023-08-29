37cea1084b379c2abbed79cff6daf896e5c3f815
Niclas Eich <niclas.eich@rwth-aachen.de>
2023-08-29 21:27:56 +0200

    took learning rate back where it was

--- utils/torch/training.py
+++ utils/torch/training.py
@@ -17,7 +17,7 @@ from scipy.special import softmax
 
 def perform_training(model, training_data, validation_data, directory, device, **kwargs):
     best_loss_val = math.inf
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, eps=1e-7)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, eps=1e-7)
     loss_fn = nn.CrossEntropyLoss(reduction="none")
     nepochs = kwargs["nepochs"]
     train_metrics = np.zeros((nepochs, 2))
