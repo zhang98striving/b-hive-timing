18c3ebc33b31da434a2fec9b70d9c19e54ab86d5
mate.farkas <mate.farkas@rwth-aachen.de>
2023-10-27 17:35:35 +0200

    Implemented DatasetConstructor chunk_size test

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,2 +1,67 @@
-def test_data_processing():
-    assert True
+import os
+import subprocess
+import unittest
+import unittest.result
+
+import numpy as np
+from rich.progress import track
+
+
+class TestDatasetConstructor(unittest.TestCase):
+    def run_dataset_constructor_for_different_chunksizes(self, chunk_sizes, output_prefix):
+        for chunk_size in chunk_sizes:
+            arguments = [
+                "law",
+                "run",
+                "DatasetConstructorTask",
+                "--test-dataset-path",
+                "minitest.txt",  # Should be adjusted to use-case
+                "--training-dataset-path",
+                "minitraining.txt",  # Should be adjusted to use-case
+                "--dataset-version",
+                f"{output_prefix}_{chunk_size}",
+                "--config",
+                "hlt_phase2",  # Should be adjusted to use-case
+                "--chunk-size",
+                str(chunk_size),
+            ]
+            subprocess.run(arguments)
+
+    def test_data_processing(self):
+        chunk_sizes = [100, 1000, 10000]
+        output_prefix = "test_miniphase2_chunk_size"
+        self.run_dataset_constructor_for_different_chunksizes(
+            chunk_sizes=chunk_sizes, output_prefix=output_prefix
+        )
+        results = np.zeros((len(chunk_sizes), 4))
+        for i, chunk_size in enumerate(chunk_sizes):
+            output = os.environ["DATA_PATH"]
+            path = (
+                output + f"/DatasetConstructorTask/{output_prefix}_{chunk_size}/processed_files.txt"
+            )
+            with open(path, "r") as file_list:
+                files = file_list.read().split("\n")
+                array = np.array([])
+                for file in track(files, f"Reading in jet_pt for chunk size {chunk_size}..."):
+                    data = np.load(file)
+                    array = np.append(array, data["global_features"]["jet_pt"])
+                res = np.array(
+                    [
+                        array.shape[0],
+                        np.round(array.sum(), 5),
+                        np.round(array.mean(), 5),
+                        np.round(array.std(), 5),
+                    ]
+                )
+                results[i] = res
+        matching_outputs = np.equal(results[0], results)
+
+        self.assertEqual(
+            True,
+            matching_outputs.all(),
+            f"Error with the dataset constructor. The created 3 datasets from the files have differing shapes, sum of jet_pt, means and standard deviations. The obtained results are: {matching_outputs}",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
