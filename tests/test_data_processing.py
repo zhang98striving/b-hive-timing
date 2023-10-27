884f4ee4ea5f30f65e98f0bb606ce5818f5530ff
mate.farkas <mate.farkas@rwth-aachen.de>
2023-10-27 17:41:16 +0200

    add documentation to the datasetconstructortest

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -8,6 +8,17 @@ from rich.progress import track
 
 
 class TestDatasetConstructor(unittest.TestCase):
+    """
+    This test module creates a dataset of chunk_size 100, 1000 and 10000 using the DatasetConstructorTask.
+    It then compares the number of samples in the created datasets, the sum, mean and standard deviation of the stored
+    jet_pt values,
+    If the values all match, the test is passed; it fails otherwise
+
+    The test can be used to check changes in the datasets, if someone tweaks with their construction.
+    It is advised to use a subset of samples, to have the three datasets created in a reasonable timeframe.
+    The dataset .txt files and config file should all be adjusted to the use-case (see run_dataset_constructor_for_different_chunksizes) function.
+    """
+
     def run_dataset_constructor_for_different_chunksizes(self, chunk_sizes, output_prefix):
         for chunk_size in chunk_sizes:
             arguments = [
