fb809eed424802bceb2d9d6eef2c6470cd4f7566
Niclas Eich <niclas2102@gmail.com>
2024-02-27 09:28:42 +0100

    Plot_loss accepting epochs

--- utils/plotting/roc.py
+++ utils/plotting/roc.py
@@ -80,16 +80,18 @@ def calculate_roc(truth, discriminator, veto, output_directory, dataset_key, nam
     return fpr, tpr, area
 
 
-def plot_losses(train_loss, test_loss, output_dir):
-    plt.title("Losses")
-    plt.plot(*np.array(list(enumerate(test_loss, 1))).T, label="Validation")
-    plt.plot(*np.array(list(enumerate(train_loss, 1))).T, label="Train")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.savefig(os.path.join(output_dir, "loss.pdf"))
-    plt.savefig(os.path.join(output_dir, "loss.png"))
-    plt.close()
+def plot_losses(train_loss, test_loss, output_dir=None, epochs=None):
+    fig, ax = plt.subplots()
+    ax.set_title("Losses")
+    if train_loss is not None:
+        ax.plot(np.linspace(0, epochs, len(train_loss)), train_loss, label="Validation", color="blue")
+    if test_loss is not None:
+        ax.plot(np.linspace(0,epochs, len(test_loss)), test_loss , label="Train", color="orange")
+    ax.set_xlabel("Iterations")
+    ax.set_ylabel("Loss")
+    ax.legend()
+    fig.savefig(os.path.join(output_dir, "loss.pdf"))
+    fig.savefig(os.path.join(output_dir, "loss.png"))
 
 
 def plot_roc(
