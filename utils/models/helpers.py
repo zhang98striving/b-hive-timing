d6983d561d34484f6da1b40d0b6ab6e3c6466969
Pavlo Kashko <pkashko@b9g57n5454.cern.ch>
2024-08-22 14:26:38 +0200

    Update DeepJet with HLT version, remove hardcoding in helpers

--- utils/models/helpers.py
+++ utils/models/helpers.py
@@ -4,16 +4,17 @@ import torch.nn as nn
 
 
 class InputConv(nn.Module):
-    def __init__(self, in_chn, out_chn, dropout_rate=0.1, **kwargs):
+    def __init__(self, in_chn, out_chn, dropout_rate=0.1, norm=True, **kwargs):
         super(InputConv, self).__init__(**kwargs)
 
+        self.norm = norm
         self.lin = torch.nn.Conv1d(in_chn, out_chn, kernel_size=1)
         self.bn = torch.nn.BatchNorm1d(out_chn, eps=0.001, momentum=0.6)
         self.act = nn.ReLU()
         self.dropout = nn.Dropout(dropout_rate)
 
-    def forward(self, x, norm=True):
-        if norm:
+    def forward(self, x):
+        if self.norm:
             x = self.dropout(self.bn(self.act(self.lin(x))))
         else:
             x = self.act(self.lin(x))
@@ -35,71 +36,59 @@ class LinLayer(nn.Module):
 
 
 class InputProcess(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, 
+                 input_dims,
+                 cpf_conv,
+                 npf_conv,
+                 vtx_conv, 
+                 **kwargs
+        ):
+        
         super(InputProcess, self).__init__(**kwargs)
 
-        self.cpf_bn = torch.nn.BatchNorm1d(16, eps=0.001, momentum=0.6)
-        self.cpf_conv1 = InputConv(16, 64)
-        self.cpf_conv2 = InputConv(64, 32)
-        self.cpf_conv3 = InputConv(32, 32)
-        self.cpf_conv4 = InputConv(32, 8)
+        cpf_conv[:0] = [input_dims[0][1]]
+        self.cpf_bn = torch.nn.BatchNorm1d(cpf_conv[0], eps=0.001, momentum=0.6)
+        self.cpf_conv = nn.ModuleList([InputConv(cpf_conv[i], cpf_conv[i+1]) for i in range(len(cpf_conv) - 2)])
+        self.cpf_conv.append(InputConv(cpf_conv[-2], cpf_conv[-1], norm=False))
 
-        self.npf_bn = torch.nn.BatchNorm1d(6, eps=0.001, momentum=0.6)
-        self.npf_conv1 = InputConv(6, 32)
-        self.npf_conv2 = InputConv(32, 16)
-        self.npf_conv3 = InputConv(16, 4)
+        npf_conv[:0] = [input_dims[1][1]]
+        self.npf_bn = torch.nn.BatchNorm1d(npf_conv[0], eps=0.001, momentum=0.6)
+        self.npf_conv = nn.ModuleList([InputConv(npf_conv[i], npf_conv[i+1]) for i in range(len(npf_conv) - 2)])
+        self.npf_conv.append(InputConv(npf_conv[-2], npf_conv[-1], norm=False))
 
-        self.vtx_bn = torch.nn.BatchNorm1d(12, eps=0.001, momentum=0.6)
-        self.vtx_conv1 = InputConv(12, 64)
-        self.vtx_conv2 = InputConv(64, 32)
-        self.vtx_conv3 = InputConv(32, 32)
-        self.vtx_conv4 = InputConv(32, 8)
+        vtx_conv[:0] = [input_dims[2][1]]
+        self.vtx_bn = torch.nn.BatchNorm1d(vtx_conv[0], eps=0.001, momentum=0.6)
+        self.vtx_conv = nn.ModuleList([InputConv(vtx_conv[i], vtx_conv[i+1]) for i in range(len(vtx_conv) - 2)])
+        self.vtx_conv.append(InputConv(vtx_conv[-2], vtx_conv[-1], norm=False))
 
     def forward(self, cpf, npf, vtx):
         cpf = self.cpf_bn(torch.transpose(cpf, 1, 2))
-        cpf = self.cpf_conv1(cpf)
-        cpf = self.cpf_conv2(cpf)
-        cpf = self.cpf_conv3(cpf)
-        cpf = self.cpf_conv4(cpf, norm=False)
+        for conv in self.cpf_conv:
+            cpf = conv(cpf)
         cpf = torch.transpose(cpf, 1, 2)
 
         npf = self.npf_bn(torch.transpose(npf, 1, 2))
-        npf = self.npf_conv1(npf)
-        npf = self.npf_conv2(npf)
-        npf = self.npf_conv3(npf, norm=False)
+        for conv in self.npf_conv:
+            npf = conv(npf)
         npf = torch.transpose(npf, 1, 2)
 
         vtx = self.vtx_bn(torch.transpose(vtx, 1, 2))
-        vtx = self.vtx_conv1(vtx)
-        vtx = self.vtx_conv2(vtx)
-        vtx = self.vtx_conv3(vtx)
-        vtx = self.vtx_conv4(vtx, norm=False)
+        for conv in self.vtx_conv:
+            vtx = conv(vtx)
         vtx = torch.transpose(vtx, 1, 2)
 
         return cpf, npf, vtx
 
 
 class DenseClassifier(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, dense_clas_dim, **kwargs):
         super(DenseClassifier, self).__init__(**kwargs)
 
-        self.LinLayer1 = LinLayer(265, 200)
-        self.LinLayer2 = LinLayer(200, 100)
-        self.LinLayer3 = LinLayer(100, 100)
-        self.LinLayer4 = LinLayer(100, 100)
-        self.LinLayer5 = LinLayer(100, 100)
-        self.LinLayer6 = LinLayer(100, 100)
-        self.LinLayer7 = LinLayer(100, 100)
-        self.LinLayer8 = LinLayer(100, 100)
+        self.LinLayers = nn.ModuleList([LinLayer(dense_clas_dim[i], dense_clas_dim[i+1]) for i in range(len(dense_clas_dim) - 1)])
 
     def forward(self, x):
-        x = self.LinLayer1(x)
-        x = self.LinLayer2(x)
-        x = self.LinLayer3(x)
-        x = self.LinLayer4(x)
-        x = self.LinLayer5(x)
-        x = self.LinLayer6(x)
-        x = self.LinLayer7(x)
-        x = self.LinLayer8(x)
 
+        for layer in self.LinLayers:
+            x = layer(x)
+        
         return x
