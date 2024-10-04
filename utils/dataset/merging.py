74984fca6fcc3aed54c11ccbac44c01347e74665
Pavlo Kashko <pavlo.kashko@cern.ch>
2024-10-04 15:51:37 +0200

    Revert "Fixed a bug with chunking and saving data from last not fully filled chunk"
    This reverts commit 03faf0f69a599162931a8266346455365ccd603c.

--- utils/dataset/merging.py
+++ utils/dataset/merging.py
@@ -106,7 +106,7 @@ def merge_datasets(
                 chunk[n_chunk : n_chunk + index_range] = data[:index_range]
                 n_chunk += index_range
                 
-                if n_chunk >= chunk_size or i == len(files)-1:
+                if n_chunk >= chunk_size:
                     filename = os.path.join(path, f"{label}_{len(file_list)}.lz4")
                     file_list.append(filename)
 
