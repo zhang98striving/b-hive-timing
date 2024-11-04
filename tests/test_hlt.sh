2c0b048fa57b24c12ebb829f49e1942902499347
Niclas Eich <niclas2102@gmail.com>
2024-11-04 10:29:44 +0100

    Divided tests into offline/hlt

--- tests/test_hlt.sh
+++ tests/test_hlt.sh
@@ -1,12 +1,15 @@
-test_version=$1
+test_version="test_hlt"
 
+# set so script aborts when something fails
 set -e
-# check if LXUSERNAME and TESTDIRECTORY are set
+
 if [ -z "$LXUSERNAME" ]; then
-    echo "LXUSERNAME is not set"
+    echo "LXUSERNAME is not set. Please set it in your environment."
+    exit 1
 fi
 if [ -z "$TESTDIRECTORY" ]; then
-    echo "TESTDIRECTORY is not set"
+    echo "TESTDIRECTORY is not set. Please set it in your environment."
+    exit 1
 fi
 mkdir -p $TESTDIRECTORY/data
 
@@ -14,9 +17,13 @@ mkdir -p $TESTDIRECTORY/data
 if [ ! -f $TESTDIRECTORY/data/hlt_test_TT.root ]; then
     scp -r $LXUSERNAME@lxplus.cern.ch:/eos/cms/store/group/phys_btag/HLT/Run3/2023_08_22/small_test.root $TESTDIRECTORY/data/hlt_test_TT.root
 fi
+
+echo "begin teardown of old tests"
+echo "a" | law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3 --model-name DeepJetHLT --epochs 1 --test-dataset-version $test_version --remove-output -1
+echo "a" | law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3 --model-name DeepJetHLT --epochs 1 --test-dataset-version $test_version --test-attack pgd --test-attack-iterations 1 --remove-output -1
 #
 # creatte test filelist
 echo "$TESTDIRECTORY/data/hlt_test_TT.root" > $TESTDIRECTORY/data/hlt_test.txt
-law run DatasetConstructorTask --dataset-version $test_version --filelist $TESTDIRECTORY/data/hlt_test.txt --coffea-worker 1 --config hlt_run3 --chunk-size 1000
-law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3 --model-name DeepJet --epochs 1 --test-dataset-version $test_version
-law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3
+law run DatasetConstructorTask --dataset-version $test_version --filelist $TESTDIRECTORY/data/hlt_test.txt --coffea-worker 4 --config hlt_run3 --chunk-size 1000
+law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3 --model-name DeepJetHLT --epochs 1 --test-dataset-version $test_version
+law run ROCCurveTask --training-version $test_version --dataset-version $test_version --config hlt_run3 --model-name DeepJetHLT --epochs 1 --test-dataset-version $test_version --test-attack pgd --test-attack-iterations 1
