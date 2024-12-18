fb5adaee6f5db842434820de8e71d1399310d4a8
Pavlo Kashko <pavlo.kashko@cern.ch>
2024-12-18 16:15:18 +0100

    Small bug fix related to the truth as dict in config

--- tasks/dataset.py
+++ tasks/dataset.py
@@ -143,7 +143,7 @@ class DatasetConstructorTask(DatasetDependency, BaseTask):
             chunk_size=self.chunk_size,
             shuffle=True,
             histograms=np.array(histograms, dtype=np.float32),
-            reference_key=config["truths"].index(config["reference_flavour"]), #reference_key is the histogram index for LZ4 dataset
+            reference_key=list(config["truths"]).index(config["reference_flavour"]), #reference_key is the histogram index for LZ4 dataset
             bins_pt=config["bins_pt"],
             bins_eta=config["bins_eta"],
             pt_key_index=pt_key_index,
